760503c21fe559ac0fd6abd74c2818740acd7d51
Abdur Rahim <adnandu2014@gmail.com>
2024-12-13 07:23:17 -0800

    cors change

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(xssClean())
 app.use(function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*'),
     res.header('Access-Control-Allow-Methods', '*'),
-    res.header('Access-Control-Allow-Headers', '*'),
+    res.header('Access-Control-Allow-Headers', 'Content-Type'),
     next();
 })
 
