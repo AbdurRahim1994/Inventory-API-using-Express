80cd638189af1a1ebdd726c4482429578e5ba1fa
Abdur Rahim <adnandu2014@gmail.com>
2024-12-21 13:55:43 -0800

    cors change

--- app.js
+++ app.js
@@ -26,12 +26,13 @@ app.use(helmet())
 app.use(hpp())
 app.use(xssClean())
 
-// app.use(function(req, res, next){
-//     res.header('Access-Control-Allow-Origin', '*'),
-//     res.header('Access-Control-Allow-Methods', '*'),
-//     res.header('Access-Control-Allow-Headers', 'Content-Type'),
-//     next();
-// })
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 
 // Body Parser Implement
 app.use(bodyParser.json())
